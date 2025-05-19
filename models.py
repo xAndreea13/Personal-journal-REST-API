d7e103784121fb09e1e3e1a5d80e8bc30ab24e05
Alina <andreeacaragea95@gmail.com>
2025-05-19 19:44:47 +0300

    Implement models classes

---
+++ models.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime
+
+# defines data models using Pydantic, 
+# which is a library used with FastAPI 
+# to validate and serialize data automatically
+
+# used for CREATING and RETURNING entries
+class JournalEntry(BaseModel):
+    title: str
+    content: str
+    tags: Optional[List[str]] = []
+    create_ad: datetime = datetime.now()
+
+# used specifically for UPDATING a journal entry
+class JournalEntryUpdate(BaseModel):
+    title: Optional[List[str]]
+    content: Optional[List[str]]
+    tags: Optional[List[str]]
